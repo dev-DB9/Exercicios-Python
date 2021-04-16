b207367fbfa1ce141d475439259c074cacc35024
Daniel Bertoldo <78268514+Daniel-Bertoldo@users.noreply.github.com>
2021-04-16 08:44:23 -0300

    modificação de nome e finalidade

---
+++ Curso em videos/Aulas.py
@@ -0,0 +1,21 @@
+#Nome: Daniel Vitor Oliveira Bertoldo
+#Professor: Gustavo Guanabara (Curso em video)
+
+#Conteudo de aula
+'''
+-----Tipos Primitivos-----
+
+int: Números Inteiros
+float: Números Reais
+Boolean: Valores Lógicos
+Str/String: Letras/Palavras/Frases
+
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite outro número: "))
+media = (numero1 + numero2)/2
+print(f"A média dos valores {numero1} e {numero2} é {media}")
+if media >= 6:
+    print("Parabéns você foi aprovado.")
+elif media < 6:
+    print("Parabéns você foi reprovado.")
+'''
